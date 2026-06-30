d8554a3d837312a12dd5df651a0ccd70f91e68fc
Rod-Belo04 <beloniardrigo@gmail.com>
2026-06-30 18:32:15 +0200

    v0.3

--- domotica_control/dash/views.py
+++ domotica_control/dash/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Thermo, Irrigazione, ConsumoElettrico
+import datetime
 
 def home(request):
     return render(request, 'dash/home.html')
@@ -7,3 +9,15 @@ def post(request):
         field = request.POST.get('field')
         if field == 'thermo':
             temp = request.POST.get('temp')
+            date = datetime.datetime.now()
+            Thermo.objects.create(temperature=temp, date=date)
+        elif field == 'irrigazione':
+            status = request.POST.get('status')
+            date = datetime.datetime.now()
+            Irrigazione.objects.create(status=status, date=date)
+        elif field == 'consumo_elettrico':
+            power = request.POST.get('power')
+            date = datetime.datetime.now()
+            ConsumoElettrico.objects.create(power=power, date=date)
+
+
